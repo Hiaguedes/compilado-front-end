35bbea50d19b8a1b7619cdcfdfa86e9e4a26e1a9
Hiaguedes <hiago_guedes94@hotmail.com>
2020-09-04 03:39:57 -0300

    implementando função de multiplicação

--- arrays/assets/js/services/soma.js
+++ arrays/assets/js/services/soma.js
@@ -1,4 +1,17 @@
 const resultado = document.querySelector('.resultado');
+const checkbox = document.querySelector('.checkbox');
+const multiplicadorCampo= document.querySelector('.multiplicador');
+let multiplicador=1;
+
+const multiplicadorLigado = ()=>{
+    if (checkbox.checked){
+        const multiplicador = multiplicadorCampo.value;
+        return multiplicador;
+    }else {
+        multiplicador =1;
+    }
+}
+
 
 export const realizarSoma = input => {
     const valor = input.value;
@@ -8,15 +21,19 @@ export const realizarSoma = input => {
         valorArray.shift();
     }
 
+    if(isNaN(valorArray[valorArray.length-1])){
+        valorArray.pop();
+    }
+
+    (multiplicadorLigado() == undefined) ? multiplicador == 1 : multiplicador = multiplicadorLigado();
+
     const calcularSoma = () => (total, numeroAtual) => total + numeroAtual;
     
     const soma = valorArray.reduce(calcularSoma(),0);
-    //console.log(valorArray);
-    if(!isNaN(soma)){
-    resultado.innerHTML = soma;
-    }
-
+    
     if(valorArray.length==1 && isNaN( valorArray[0])){
         resultado.innerHTML = 0;
     }
+
+    resultado.innerHTML = multiplicador*soma;
 }
